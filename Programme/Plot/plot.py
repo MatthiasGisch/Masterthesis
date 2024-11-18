b566112998dae445a88f1bf033c647cb1a528dd6
Matthias Gisch <s17ad9@umwelt-campus.de>
2024-11-18 14:21:27 +0100

    wip

--- Programme/Plot/plot.py
+++ Programme/Plot/plot.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 # Load the dataset into a Pandas DataFrame
-df = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/13.11.2024 Roboterzelle/Testmessung2/Leer.csv')
-df2 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/13.11.2024 Roboterzelle/Testmessung2/Wasser.csv')
+df = pd.read_csv('Messungen/PU-Schaum BASF 18.11.2024/644-2.csv', sep=';')
+df2 = pd.read_csv('Messungen/PU-Schaum BASF 18.11.2024/645-2.csv', sep=';')
 df3 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/13.11.2024 Roboterzelle/Testmessung2/WasserEssig.csv')
 df4 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/13.11.2024 Roboterzelle/Testmessung2/Papierspritzguss.csv')
 
@@ -12,16 +12,16 @@ measurements = df['Measurement']
 time = df['Time']
 measurements2 = df2['Measurement']
 time2 = df2['Time']
-measurements3 = df3['Measurement']
-time3 = df3['Time']
-measurements4 = df4['Measurement']
-time4 = df4['Time']
+#measurements3 = df3['Measurement']
+#time3 = df3['Time']
+#measurements4 = df4['Measurement']
+#time4 = df4['Time']
 
 # Create a line plot
-plt.plot(time, measurements, color = 'blue', label = 'Luft')
-plt.plot(time, measurements2, color = 'red', label = 'Wasser')
-plt.plot(time, measurements3, color = 'green', label = 'WasserEssig')
-plt.plot(time, measurements4, color = 'yellow', label = 'Papierspritzguss')
+plt.plot(time, measurements, color = 'blue', label = '644-2')
+plt.plot(time, measurements2, color = 'red', label = '645-2')
+#plt.plot(time, measurements3, color = 'green', label = 'WasserEssig')
+#plt.plot(time, measurements4, color = 'yellow', label = 'Papierspritzguss')
 plt.grid(True)
 
 plt.ylabel('Messwert')
