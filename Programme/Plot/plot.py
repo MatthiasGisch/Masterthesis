040c8e5a5b3a4cba2e9c72def6076f3faffe0389
Matthias Gisch <s17ad9@umwelt-campus.de>
2024-11-25 15:39:59 +0100

    wip

--- Programme/Plot/plot.py
+++ Programme/Plot/plot.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 
 # Load the dataset into a Pandas DataFrame
-df = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/PU Schaum BASF 18.11.2024 Durchgang 2/645-2.csv')
-df2 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/PU Schaum BASF 18.11.2024 Durchgang 2/645-2.2.csv')
-df3 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/PU Schaum BASF 18.11.2024 Durchgang 2/644-2.csv')
-df4 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/PU Schaum BASF 18.11.2024 Durchgang 2/644-2.2.csv')
+df = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/BASF 25.11.2024/644-2.csv')
+df2 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/BASF 25.11.2024/644-8.csv')
+df3 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/BASF 25.11.2024/645-1.csv')
+df4 = pd.read_csv('C:/Users/matth/Desktop/Masterthesis/Messungen/BASF 25.11.2024/645-2.csv')
 
 measurements = df['Measurement']
 time = df['Time']
@@ -18,10 +18,10 @@ measurements4 = df4['Measurement']
 time4 = df4['Time']
 
 # Create a line plot
-plt.plot(time, measurements, color = 'blue', label = '645-2')
-plt.plot(time2, measurements2, color = 'red', label = '645-2.2')
-plt.plot(time3, measurements3, color = 'green', label = '644-2')
-plt.plot(time4, measurements4, color = 'yellow', label = '644-2.2')
+plt.plot(time, measurements, color = 'blue', label = '644-2')
+plt.plot(time2, measurements2, color = 'red', label = '644-8')
+plt.plot(time3, measurements3, color = 'green', label = '645-1')
+plt.plot(time4, measurements4, color = 'yellow', label = '645-2')
 plt.grid(True)
 
 plt.ylabel('Messwert')
